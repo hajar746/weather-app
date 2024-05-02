6c5fcd5543799a991879378b40ffc93c6368d44b
hajar746 <132266440+hajar746@users.noreply.github.com>
2024-05-02 14:42:24 +0300

    add UI

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import "./style.css";
-import { getWeatherForecast, handleError } from "./getData";
+import { getWeatherForecast } from "./getData";
+
+const btnSearch = document.querySelector(".btn-search");
+const location = document.querySelector("#searchLocation");
+
+btnSearch.addEventListener("click", function (e) {
+  e.preventDefault();
+  getWeatherForecast(location.value).then((res) => {
+    console.log(res.location);
+  });
+  location.value = "";
+});
