9c189dc95a82d12b8553d334334dd7b9fb27b3ea
hajar746 <132266440+hajar746@users.noreply.github.com>
2024-05-02 19:02:33 +0300

    put weather data into html elements

--- src/getData.js
+++ src/getData.js
@@ -10,3 +10,65 @@ export async function getWeatherForecast(location) {
     console.log(err);
   }
 }
+
+// MAKE WEATHER PAGE USING FETCHED DATA
+export function makePage(data, div) {
+  div.insertAdjacentHTML(
+    "afterbegin",
+    `
+       <main class="weather">
+      <form action="#" class="form-search-2">
+        <input
+          type="text"
+          name="location"
+          id="searchLocation2"
+          placeholder="${data.location.name}"
+          required
+        />
+      </form>
+
+      <div class="temp">
+        <h1 class="temperature">${data.current.temp_c}</h1>
+         <img src="${data.current.condition.icon}" alt="temp-icon" />
+      <p>${data.current.condition.text}</p>
+      </div>
+      <div class="imp-info">
+        <h3 class="city">${data.location.name}, ${data.location.country}</h3>
+        <p>${data.forecast.forecastday[0].date}</p>
+      </div>
+      <div class="other-info">
+        <div class="feels-like">
+          <p>Feels like</p>
+          <p>${data.current.feelslike_c}</p>
+        </div>
+        <div class="chance">
+          <p>Chance of rain</p>
+          <p>${data.forecast.forecastday[0].day.daily_chance_of_rain}</p>
+        </div>
+        <div class="humidity">
+          <p>Humidity</p>
+          <p>${data.current.humidity}</p>
+        </div>
+        <div class="wind">
+          <p>Wind</p>
+          <p>${data.forecast.forecastday[0].day.maxwind_kph}</p>
+        </div>
+      </div>
+      <div class="more-info">
+        <div class="high-temp">
+          <p>Highest temp</p>
+          <p>${data.forecast.forecastday[0].day.maxtemp_c}</p>
+        </div>
+        <div class="low-temp">
+          <p>Lowest temp</p>
+          <p>${data.forecast.forecastday[0].day.mintemp_c}</p>
+        </div>
+        <div class="uv">
+          <p>UV index</p>
+          <p>${data.current.uv}</p>
+        </div>
+      </div>
+    </main>
+`
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import "./style.css";
-import { getWeatherForecast } from "./getData";
+import { getWeatherForecast, makePage } from "./getData";
 
 const btnSearch = document.querySelector(".btn-search");
 const location = document.querySelector("#searchLocation");
+const homePage = document.querySelector(".homepage");
 
 btnSearch.addEventListener("click", function (e) {
   e.preventDefault();
   getWeatherForecast(location.value).then((res) => {
-    console.log(res.location);
+    makePage(res, document.body);
   });
   location.value = "";
+  homePage.classList.toggle("hide");
 });
