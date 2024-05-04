f00f77b26506b3c9fe0c3cca77ad056229c09caf
hajar746 <132266440+hajar746@users.noreply.github.com>
2024-05-04 17:56:17 +0300

    fix UI and add hourly forecast

--- src/getData.js
+++ src/getData.js
@@ -14,61 +14,93 @@ export async function getWeatherForecast(location) {
 // MAKE WEATHER PAGE USING FETCHED DATA
 export function makePage(data, div) {
   div.insertAdjacentHTML(
-    "afterbegin",
+    "beforeend",
     `
-       <main class="weather">
-      <form action="#" class="form-search-2">
-        <input
-          type="text"
-          name="location"
-          id="searchLocation2"
-          placeholder="${data.location.name}"
-          required
-        />
-      </form>
-
+    <div class = 'main'>
       <div class="temp">
-        <h1 class="temperature">${data.current.temp_c}</h1>
+      <div class='temp-info'>
+      <h1 class="temperature">${data.current.temp_c}</h1>
          <img src="${data.current.condition.icon}" alt="temp-icon" />
-      <p>${data.current.condition.text}</p>
+          <h3>${data.current.condition.text}</h3>
       </div>
-      <div class="imp-info">
-        <h3 class="city">${data.location.name}, ${data.location.country}</h3>
-        <p>${data.forecast.forecastday[0].date}</p>
+          <div class= 'city'>
+            <h3 class="city-title">${data.location.name}, ${data.location.country}</h3>
+          </div>
       </div>
-      <div class="other-info">
-        <div class="feels-like">
-          <p>Feels like</p>
+      </div>
+      <div class='div-weather-info'>
+      <div class="weather-info">
+        <div class="feels-like info">
+          <h3>Feels like</h3>
           <p>${data.current.feelslike_c}</p>
         </div>
-        <div class="chance">
-          <p>Chance of rain</p>
+        <div class="chance info">
+          <h3>Chance of rain</h3>
           <p>${data.forecast.forecastday[0].day.daily_chance_of_rain}</p>
         </div>
-        <div class="humidity">
-          <p>Humidity</p>
+        <div class="humidity info">
+          <h3>Humidity</h3>
           <p>${data.current.humidity}</p>
         </div>
-        <div class="wind">
-          <p>Wind</p>
+        <div class="wind info">
+          <h3>Wind</h3>
           <p>${data.forecast.forecastday[0].day.maxwind_kph}</p>
         </div>
       </div>
-      <div class="more-info">
-        <div class="high-temp">
-          <p>Highest temp</p>
+      <div class="weather-info">
+        <div class="high-temp info">
+          <h3>Highest temp</h3>
           <p>${data.forecast.forecastday[0].day.maxtemp_c}</p>
         </div>
-        <div class="low-temp">
-          <p>Lowest temp</p>
+        <div class="low-temp info">
+          <h3>Lowest temp</h3>
           <p>${data.forecast.forecastday[0].day.mintemp_c}</p>
         </div>
-        <div class="uv">
-          <p>UV index</p>
+        <div class="uv info">
+          <h3>UV index</h3>
           <p>${data.current.uv}</p>
         </div>
       </div>
-    </main>
+      </div>
+      <div class="weather">
+      <div class="hourly">
+        <h2>Hourly Forecast</h2>
+        <ul class="hourly-divs">
+          <li class="hourly-div">
+            <p>${data.forecast.forecastday[0].hour[7].time}</p>
+            <img src="${data.forecast.forecastday[0].hour[7].condition.icon}" alt="" />
+          </li>
+          <li class="hourly-div">
+            <p>${data.forecast.forecastday[0].hour[8].time}</p>
+            <img src="${data.forecast.forecastday[0].hour[8].condition.icon}" alt="" />
+          </li>
+          <li class="hourly-div">
+            <p>${data.forecast.forecastday[0].hour[9].time}</p>
+            <img src="${data.forecast.forecastday[0].hour[9].condition.icon}" alt="" />
+          </li>
+          <li class="hourly-div">
+            <p>${data.forecast.forecastday[0].hour[10].time}</p>
+            <img src="${data.forecast.forecastday[0].hour[10].condition.icon}" alt="" />
+          </li>
+          <li class="hourly-div">
+            <p>${data.forecast.forecastday[0].hour[11].time}</p>
+            <img src="${data.forecast.forecastday[0].hour[11].condition.icon}" alt="" />
+          </li>
+          <li class="hourly-div">
+            <p>${data.forecast.forecastday[0].hour[12].time}</p>
+            <img src="${data.forecast.forecastday[0].hour[12].condition.icon}" alt="" />
+          </li>
+          <li class="hourly-div">
+            <p>${data.forecast.forecastday[0].hour[13].time}</p>
+            <img src="${data.forecast.forecastday[0].hour[13].condition.icon}" alt="" />
+          </li>
+          <li class="hourly-div">
+            <p>${data.forecast.forecastday[0].hour[14].time}</p>
+            <img src="${data.forecast.forecastday[0].hour[14].condition.icon}" alt="" />
+          </li>
+        </ul>
+      </div>
+    </div>
 `
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import { getWeatherForecast, makePage } from "./getData";
 
 const btnSearch = document.querySelector(".btn-search");
 const location = document.querySelector("#searchLocation");
-const homePage = document.querySelector(".homepage");
+const weatherDiv = document.querySelector(".weather");
+
+getWeatherForecast("London").then((res) => {
+  makePage(res, weatherDiv);
+});
 
 btnSearch.addEventListener("click", function (e) {
+  weatherDiv.textContent = "";
   e.preventDefault();
   getWeatherForecast(location.value).then((res) => {
-    makePage(res, document.body);
+    makePage(res, weatherDiv);
   });
-  location.value = "";
-  homePage.classList.toggle("hide");
 });
